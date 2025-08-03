4ca54c27bda4f755cc3e569947ef3c197855d978
JostinVega <jostin.vega@epn.edu.ec>
2025-08-03 18:40:20 -0500

    Actualización de ruta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:8080/api'
-  apiUrl: 'http://taskflowbackend-dev.us-east-1.elasticbeanstalk.com/api'
+  apiUrl: 'http://taskflowbackend-env.eba-rihpxgmp.us-east-1.elasticbeanstalk.com/api'
+  //apiUrl: 'http://taskflowbackend-dev.us-east-1.elasticbeanstalk.com/api'
+  //apiUrl: 'http://taskflow-backend.eba-rpfthsfa.us-east-1.elasticbeanstalk.com/api'
 };
